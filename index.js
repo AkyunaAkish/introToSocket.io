8f97897306f78cdf8d85d1e12354e3897635fa57
AkyunaAkish <akyunaakish@gmail.com>
2015-09-01 15:11:57 -0600

    added finished code

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 //Your code BELOW
+io.on('connection', function(socket){
+  console.log('connected');
+  socket.on('disconnect', function(){
+    console.log('disconnected');
+  })
+})
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
+  })
+})
+
+
 
 
 
